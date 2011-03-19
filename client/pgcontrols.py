2743dd7a54640fb264720a4297dff20d264f1462
chubakur <chubakur@gmail.com>
2011-03-19 16:25:08 +0300

    PGControls update

--- client/pgcontrols.py
+++ client/pgcontrols.py
@@ -1,26 +1,13 @@
-import pygame.sprite
-#PGControls 
-#Copyright (C) 2011 chubakur@gmail.com
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
-
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import pygame
+import pygame.sprite
 pygame.font.init()
 font = pygame.font.Font("misc/Domestic_Manners.ttf", 10)
+font2 = pygame.font.Font('misc/Domestic_Manners.ttf', 30)
 group = pygame.sprite.Group()
 class Button(pygame.sprite.Sprite):
-    def __init__(self,parent,rect,func,label,group_u = False,):
+    def __init__(self, parent, rect, func, label, group_u=False):
         pygame.sprite.Sprite.__init__(self)
+        self.font = font
         self.type = "button"
         self.button_image = pygame.image.load('misc/pgcontrols/button.png')
         self.button_pressed_image = pygame.image.load('misc/pgcontrols/button_pressed.png')
@@ -30,6 +17,8 @@ class Button(pygame.sprite.Sprite):
         self.parent = parent
         self.func = func
         self.pressed = 0
+        self.label_text = label
+        self.color = (255, 255, 255)
         self.render_label(label)
         self.impose_label()
         if not group_u:
@@ -39,33 +28,86 @@ class Button(pygame.sprite.Sprite):
         self.group.add(self)
         self.shown = True
     def hide(self):
-        self.shown = False
-        self.group.remove(self)
+        if self.shown:
+            self.shown = False
+            self.group.remove(self)
+        else:
+            return
     def show(self):
-        self.shown = True
-        self.group.add(self)
+        if not self.shown:
+            self.shown = True
+            self.group.add(self)
+        else:
+            return
     def press(self):
         self.pressed = 1
     def pull(self):
         self.pressed = 0
         self.func()
     def impose_label(self):
-        self.button_image.blit(self.label,(self.button_image.get_size()[0]/2-self.label.get_size()[0]/2,self.button_image.get_size()[1]/2-self.label.get_size()[1]/2))
-        self.button_pressed_image.blit(self.label,(self.button_image.get_size()[0]/2-self.label.get_size()[0]/2,self.button_image.get_size()[1]/2-self.label.get_size()[1]/2))
-        #self.button_image_backup = self.button_image.copy()
-        #self.button_pressed_image_backup = self.button_pressed_image.copy()
-    def change_label(self, label):
-        #self.label = label
         self.button_image = self.button_image_backup.copy()
         self.button_pressed_image = self.button_pressed_image_backup.copy()
+        self.button_image.blit(self.label, (self.button_image.get_size()[0] / 2-self.label.get_size()[0] / 2, self.button_image.get_size()[1] / 2-self.label.get_size()[1] / 2))
+        self.button_pressed_image.blit(self.label, (self.button_image.get_size()[0] / 2-self.label.get_size()[0] / 2, self.button_image.get_size()[1] / 2-self.label.get_size()[1] / 2))
+        #self.button_image_backup = self.button_image.copy()
+        #self.button_pressed_image_backup = self.button_pressed_image.copy()
+    def set_label(self, label):
+        #self.label = label
+        #self.button_image = self.button_image_backup.copy()
+        #self.button_pressed_image = self.button_pressed_image_backup.copy()
+        self.label_text = label
         self.render_label(label)
         self.impose_label()
     def render_label(self, label):
-        self.label = font.render(label,True,(255,255,255))
+        self.label = self.font.render(label, True, self.color)
+    def set_color(self, color):
+        self.color = color
+        self.render_label(self.label_text)
+        self.impose_label()
     def draw(self):
         if not self.pressed:
-            self.parent.blit(self.button_image,self.rect)
+            self.parent.blit(self.button_image, self.rect)
         else:
-            self.parent.blit(self.button_pressed_image,self.rect)
+            self.parent.blit(self.button_pressed_image, self.rect)
+    def update(self):
+        self.draw()
+class Label(pygame.sprite.Sprite):
+    def __init__(self, parent, rect, label, group_u=False):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = font2
+        self.parent = parent
+        self.color = (255, 255, 255)
+        self.label_text = label
+        #self.label = self.font.render(label, True, self.color)
+        self.render_label()
+        self.rect = self.label.get_rect().move(rect)
+        self.shown = True
+        if not group_u:
+            self.group = group
+        else:
+            self.group = group_u
+        self.group.add(self)
+    def render_label(self):
+        self.label = self.font.render(self.label_text, True, self.color)
+    def set_color(self, color):
+        self.color = color
+        self.render_label()
+    def set_label(self, label):
+        self.label_text = label
+        self.render_label()
+    def show(self):
+        if not self.shown:
+            self.shown = True
+            self.group.add(self)
+        else:
+            return
+    def hide(self):
+        if self.shown:
+            self.shown = False
+            self.group.remove(self)
+        else:
+            return
+    def draw(self):
+        self.parent.blit(self.label, self.rect)
     def update(self):
         self.draw()
--- client/test.py
+++ client/test.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-import pygame.sprite
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-import pygame
 import sys
-from pygame.locals import *
+
 import pgcontrols
+import pygame
+from pygame.locals import *
+import pygame.sprite
 screen = pygame.display.set_mode((800, 600))
 background = pygame.Surface(screen.get_size())
 background = background.convert()
@@ -21,32 +20,45 @@ class Point(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('misc/point_alpha.gif').convert_alpha()
         self.rect = pygame.Rect
-    def draw(self,rect):
+    def draw(self, rect):
         background.blit(self.image, rect)
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(rect)
 point = Point()
-btn1 = pgcontrols.Button(background,(0,0),btn1_action,'QUIT')
+label = pgcontrols.Label(background, (100,100),'It`s LABEL!')
+#label.set_label('Fuck!')
+btn1 = pgcontrols.Button(background, (0, 0), btn1_action, 'QUIT')
 def btn2_action():
     if btn1.shown:
         btn1.hide()
     else:
         btn1.show()
-btn2 = pgcontrols.Button(background,(100,0),btn2_action,'SQ')
+btn2 = pgcontrols.Button(background, (100, 0), btn2_action, 'SQ')
 a = 0
 def switch_language():
     global a
     if not a:
-        btn1.change_label('EXIT')
-        btn2.change_label('PSV')
-        btn3.change_label('US')
+        label.set_color((255,255,255))
+        btn1.set_label('EXIT')
+        btn2.set_label('PSV')
+        btn3.set_label('US')
+        btn3.set_color((255, 0, 0))
         a = 1
     else:
-        btn1.change_label('QUIT')
-        btn2.change_label('SQ')
-        btn3.change_label('RU')
+        label.set_color((25,25,0))
+        btn1.set_label('QUIT')
+        btn2.set_label('SQ')
+        btn3.set_label('RU')
+        btn3.set_color((0, 255, 0))
         a = 0
-btn3 = pgcontrols.Button(background,(200,0),switch_language,'RU')
+btn3 = pgcontrols.Button(background, (200, 0), switch_language, 'RU')
+btn3.set_color((0, 255, 0))
+def show_hide_label():
+    if label.shown:
+        label.hide()
+    else:
+        label.show()
+btn4 = pgcontrols.Button(background, (300,0), show_hide_label, 'LABEL')
 while 1:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -66,6 +78,6 @@ while 1:
                 if item.type == "button":
                     item.pull()
         pgcontrols.group.update()
-        screen.blit(background,(0,0))
-        background.fill((0,0,0))
+        screen.blit(background, (0, 0))
+        background.fill((0, 0, 0))
         pygame.display.flip()
