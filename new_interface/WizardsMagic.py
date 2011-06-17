6b4a99984fae8a483f5c817aa4a3762bb2e903e4
chubakur <chubakur@gmail.com>
2011-06-17 20:08:42 +0400

    Fixed result of merge

--- new_interface/WizardsMagic.py
+++ new_interface/WizardsMagic.py
@@ -276,8 +276,8 @@ clock = pygame.time.Clock()
 
 #read configuration file
 options.read_configuration()
-if globals.sound == "Y":
-    globals.bg_sound = pygame.mixer.Sound(current_folder+'/misc/sounds/11_the_march_of_the_goblins__tobias_steinmann.ogg')
+globals.bg_sound = pygame.mixer.Sound(current_folder+'/misc/sounds/11_the_march_of_the_goblins__tobias_steinmann.ogg')
+if globals.music == "Y":
     globals.bg_sound.play(-1)
 menu.menu_main()
 
